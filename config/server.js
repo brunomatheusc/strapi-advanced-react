7b96050d438ce34200c0277ee348a7a486590a36
Bruno Matheus <bmatheusc@gmail.com>
2021-04-16 15:51:13 -0300

    Adding first components

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3275c4f124fc72f46531831ea13cfbae'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b23669c6-9a9d-4d86-a7f2-92e2585a3b9d'
+};
